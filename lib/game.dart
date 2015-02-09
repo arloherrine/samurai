a05bd7d15435b41fbd81cdffe70dfa0c57dfbb46
arlo <arlo@indeed.com>
2015-02-09 11:06:01 -0600

    really fixing remaining errors in lib

--- lib/game.dart
+++ lib/game.dart
@@ -7,12 +7,14 @@ final int MAX_HAND_SIZE = 7;
 class Game {
 
   final List<Player> players = new List();
+  final Interface interface;
   int currentTurn = 0;
   int currentActions = 0;
   bool isAnyoneShogun = false;
   List<Card> deck;
   List<Card> discard = new List();
-  List<String> commandHistory = new List();
+
+  Game(this.interface);
 
   bool doTurn() {
     Player player = players[currentTurn % players.length];
@@ -26,17 +28,8 @@ class Game {
     }
 
     while (currentActions < 5) {
-      Action action = player.human.getAction();
-      if (action.playerIndex != currentTurn % players.length) {
-        player.human.alert("You can only submit moves for yourself!");
-      } else {
-        try {
-          currentActions += action.perform(deck, discard, players, (player.getKi() ~/ 3) - currentActions);
-          // TODO now that we know command is valid, update command history list and update players
-        } on InvalidActionException catch (e) {
-          player.human.alert(e.msg);
-        }
-      }
+      Action action = interface.getAction(currentTurn % players.length, players, (player.getKi() ~/ 3) - currentActions);
+      currentActions += action.perform(deck, discard, players, interface);
       if (deck.isEmpty) {
         var tmp = deck;
         deck = discard;
@@ -69,9 +62,6 @@ class Game {
     //TODO put initial state into command history
 
     while(!doTurn());
-
-    for (Player player in players) {
-      player.human.alert("Game over, player " + (currentTurn % players.length).toString() + " wins! Thanks for playing");
-    }
+    interface.update("win " + (currentTurn % players.length).toString());
   }
 }
